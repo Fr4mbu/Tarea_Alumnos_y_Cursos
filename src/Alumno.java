e2933ced2221ad26e900ef678d49e9347e838954
Fr4mbu <f.aguayo03@ufromail.cl>
2025-09-30 21:54:26 -0300

    Implementar constructores de Alumno con y sin parametros

--- src/Alumno.java
+++ src/Alumno.java
@@ -5,4 +5,21 @@ public class Alumno {
     private String matricula;
     private String carrera;
     private int edad;
+
+    //constructor sin parametros
+    public Alumno() {
+        this.nombre = "";
+        this.apellido = "";
+        this.matricula = "";
+        this.carrera = "";
+        this.edad = 0;
+    }
+
+    public Alumno(String nombre, String apellido, String matricula, String carrera,  int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.matricula = matricula;
+        this.carrera = carrera;
+        this.edad = edad;
+    }
 }
