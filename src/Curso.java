1f14796edeeb9c3294c4eda9809cbb09689460fe
Fr4mbu <f.aguayo03@ufromail.cl>
2025-10-01 17:57:16 -0300

    Implementacion constructores y metodos basicos de gestion para Curso

--- src/Curso.java
+++ src/Curso.java
@@ -5,4 +5,44 @@ public class Curso {
     private String codigoCurso;
     private ArrayList<Alumno> alumnos;
 
+    //constructor sin parametros
+    public Curso() {
+        this.nombreCurso = "";
+        this.codigoCurso = "";
+        this.alumnos = new ArrayList<>();
+    }
+
+    //constructor con parametros
+    public Curso(String nombreCurso, String codigoCurso) {
+        this.nombreCurso = nombreCurso;
+        this.codigoCurso = codigoCurso;
+        this.alumnos = new ArrayList<>();
+    }
+
+    //metodo agregar estudiante
+    public void agregarAlumno(Alumno a) {
+        for (Alumno alumno : alumnos) {
+            if (alumno.getMatricula().equals(a.getMatricula())) {
+                System.out.println("El alumno con matricula " + a.getMatricula() + " ya existe");
+                return;
+            }
+        }
+        alumnos.add(a);
+    }
+
+    //metodo para eliminar estudiantes
+    public void eliminarAlumno(String matricula) {
+        alumnos.removeIf(alumno -> alumno.getMatricula().equals(matricula));
+    }
+
+    //metodo para verdificar
+    public boolean perteneceCurso(String matricula) {
+        for (Alumno alumno : alumnos) {
+            if (alumno.getMatricula().equals(matricula)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
